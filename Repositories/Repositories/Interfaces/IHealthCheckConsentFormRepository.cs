fbe0b36c788c6ddbaee54c778dbfad51ee9213ca
pthaison2004 <sonptse180048@fpt.edu.vn>
2025-07-09 12:03:06 +0700

    Add files via upload

---
+++ Repositories/Repositories/Interfaces/IHealthCheckConsentFormRepository.cs
@@ -0,0 +1,17 @@
+using Businessobjects.Models;
+
+namespace Repositories.Interfaces
+{
+    public interface IHealthCheckConsentFormRepository
+    {
+        Task<IEnumerable<HealthCheckConsentForm>> GetAllConsentFormsAsync();
+        Task<HealthCheckConsentForm?> GetConsentFormByIdAsync(string id);
+        Task<IEnumerable<HealthCheckConsentForm>> GetConsentFormsByPlanIdAsync(string planId);
+        Task<IEnumerable<HealthCheckConsentForm>> GetConsentFormsByStudentIdAsync(string studentId);
+        Task<HealthCheckConsentForm?> GetConsentFormByPlanAndStudentAsync(string planId, string studentId);
+        Task CreateConsentFormAsync(HealthCheckConsentForm form);
+        Task UpdateConsentFormAsync(HealthCheckConsentForm form);
+        Task DeleteConsentFormAsync(string id);
+        Task<bool> ConsentFormExistsAsync(string id);
+    }
+}
